01b6a10dae58ac4b03fb4576a946b4267c0fdc45
dineshk033 <dineshk033@gmail.com>
2020-06-22 15:04:54 +0530

    Search bar

--- side-nav/side-nav.component.ts
+++ side-nav/side-nav.component.ts
@@ -8,7 +8,17 @@ import { MENU_TREE } from './side-nav.model';
 })
 export class SideNavComponent implements OnInit {
   menuItem = MENU_TREE;
+  searchMenuItem;
+  searchValue = '';
   constructor() {}
 
   ngOnInit(): void {}
+  menuSearch(arg) {
+    this.searchMenuItem = this.menuItem.map((el) => {
+      return el.children.filter((cel) =>
+        cel.name.toLowerCase().includes(this.searchValue.toLowerCase())
+      );
+    });
+    this.searchMenuItem = this.searchMenuItem.flat();
+  }
 }
