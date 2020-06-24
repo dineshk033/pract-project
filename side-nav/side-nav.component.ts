8eba24f2b69529bda8a964baa4e29467a4473356
dineshk033 <dineshk033@gmail.com>
2020-06-24 13:54:44 +0530

    menu update 24/06

--- side-nav/side-nav.component.ts
+++ side-nav/side-nav.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { BreakpointObserver, MediaMatcher } from '@angular/cdk/layout';
+
+import { FormControl } from '@angular/forms';
 import { MENU_TREE } from './side-nav.model';
 
 @Component({
@@ -7,18 +10,41 @@ import { MENU_TREE } from './side-nav.model';
   styleUrls: ['./side-nav.component.css'],
 })
 export class SideNavComponent implements OnInit {
-  menuItem = MENU_TREE;
-  searchMenuItem;
-  searchValue = '';
-  constructor() {}
+  public selectedItem: string;
+  public toggleIcon: boolean;
+  public mobileQuery: MediaQueryList;
+  public searchMenuItem;
+  public searchValue = new FormControl('');
+  private _mobileQueryListener: () => void;
+  public menuItem = MENU_TREE;
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.toggleIcon = true;
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
   menuSearch(arg) {
     this.searchMenuItem = this.menuItem.map((el) => {
       return el.children.filter((cel) =>
-        cel.name.toLowerCase().includes(this.searchValue.toLowerCase())
+        cel.name.toLowerCase().includes(this.searchValue.value.toLowerCase())
       );
     });
     this.searchMenuItem = this.searchMenuItem.flat();
+  }
+
+  clearSearch() {
+    this.searchValue.setValue('');
   }
 }
